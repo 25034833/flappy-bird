94f7492659a27efc5d24a89b0614379b54a3bac9
Abdul Moiz <abdulmoiz89355@gmail.com>
2025-11-14 12:46:45 +0000

    Add bird object with shape and starting position

--- game.py
+++ game.py
@@ -15,3 +15,11 @@ game_over = False
 gravity = -0.3
 bird_velocity = 0
 game_started = False
+# Bird
+bird = turtle.Turtle()
+bird.speed(0)
+bird.shape("circle")
+bird.color("yellow")
+bird.shapesize(1.5, 1.5)
+bird.penup()
+bird.goto(-200, 0)
